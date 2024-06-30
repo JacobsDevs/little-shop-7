4b1aa6e378f29939f60b2bf63846f0c865dcd730
Jacob <jacobphelan@hotmail.com>
2024-06-30 19:25:59 +1000

    add: Feature Merchant Items Show page

--- spec/features/merchant/items_spec.rb
+++ spec/features/merchant/items_spec.rb
@@ -1,17 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe 'Merchant Items page' do
-	it 'has an index page with just that Merchant\'s Items' do
-		merchant = Merchant.find(1)
-		items = merchant.items
-
-		visit "merchants/#{merchant.id}/items"
-
-		save_and_open_page
-		items.each do |item|
-			expect(page).to have_content(item.name)
+	describe 'Merchant Items Index' do
+		it 'Has just that Merchant\'s Items' do
+			merchant = Merchant.find(1)
+			items = merchant.items
+			
+			visit "merchants/#{merchant.id}/items"
+			
+			items.each do |item|
+				expect(page).to have_content(item.name)
+			end
+			
+			expect(page).to_not have_content(Item.find(16).name)
 		end
+		it 'Each item name is a link to that item show page' do
+			merchant = Merchant.find(1)
 
-		expect(page).to_not have_content(Item.find(16).name)
+			visit "/merchants/#{merchant.id}/items"
+
+			click_link "#{merchant.items.first.name}"
+
+			expect(current_path).to eq("/merchants/#{merchant.id}/items/#{merchant.items.first.id}")
+			expect(page).to have_content("Name: #{merchant.items.first.name}")
+			expect(page).to have_content("Description: #{merchant.items.first.description}")
+			expect(page).to have_content("Price: #{merchant.items.first.unit_price}")
+		end
 	end
 end
